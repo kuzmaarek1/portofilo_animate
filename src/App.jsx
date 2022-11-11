55f9d0d8a9086c157b2dce6014947b2ac3372a25
arm650 <arm650>
2022-11-11 15:54:15 +0100

    Add animate to pagges

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Layout from './components/Layout'
+//import Portfolio from './components/Portfolio'
+//import Dashboard from './components/Dashboard'
+import './App.scss'
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            {/*
+            <Route path="/portfolio" element={<Portfolio />} />           
+            <Route path="/dashboard" element={<Dashboard />} />
+            */}
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
